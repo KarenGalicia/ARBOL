8bd613d5efeb0e2e1e3cbf4237fcc9156939b17d
KarenGalicia <kjimenezg6@miumg.edu.gt>
2025-03-22 21:21:08 -0600

    Initial commit

---
+++ src/main/java/umg/progra2/ArbolCadena/ArbolCadena.java
@@ -0,0 +1,2 @@
+package umg.progra2.ArbolCadena;public class ArbolCadena {
+}
---
+++ src/main/java/umg/progra2/Datos/Personas.java
@@ -0,0 +1,18 @@
+package umg.progra2.Datos;
+
+public class Personas
+{
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public int numeroTelefono;
+    private String Nombre;
+    private String Direccion;
+}
+
+        
